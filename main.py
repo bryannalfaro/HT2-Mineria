55c7cdeaabf9c53cb949d2bf36fab9c30b28d84c
Bryann Alfaro <alf19372@uvg.edu.gt>
2022-02-15 18:04:48 -0600

    feat: empezando algoritmos

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 #Diego de Jesus
 #Julio Herrera
 
+#Referencia: https://www.youtube.com/watch?v=s6PSSzeUMFk&t=1044s
+
 from math import ceil
 import pandas as pd
 import numpy as np
@@ -18,8 +20,11 @@ from sklearn import datasets
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import silhouette_samples
+from sklearn.decomposition import PCA
 import pyclustertend
 import random
+import scipy.cluster.hierarchy as sch
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 from clean import *
 # movies = pd.read_csv('movies.csv', encoding='unicode_escape')
 movies = pd.read_csv('movies.csv', encoding='latin1', engine='python')
@@ -59,7 +64,7 @@ movies_df_clean = movies_clean_norm.fillna(0)
 
 print(movies_df_clean.describe())
 
-#Analisis de tendencia a agrupamiento
+'''#Analisis de tendencia a agrupamiento
 
 #Metodo Hopkings
 
@@ -83,4 +88,38 @@ plt.plot(range(1, 11), wcss)
 plt.title('Grafico de codo')
 plt.xlabel('No. Clusters')
 plt.ylabel('Puntaje')
+plt.show()'''
+
+'''clusters=  KMeans(n_clusters=3, max_iter=300) #Creacion del modelo
+clusters.fit(movies_df_clean) #Aplicacion del modelo de cluster
+
+movies_df_clean['cluster'] = clusters.labels_ #Asignacion de los clusters
+print(movies_df_clean.head())
+
+pca = PCA(2)
+pca_movies = pca.fit_transform(movies_df_clean)
+pca_movies_df = pd.DataFrame(data = pca_movies, columns = ['PC1', 'PC2'])
+pca_clust_movies = pd.concat([pca_movies_df, movies_df_clean[['cluster']]], axis = 1)
+
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(1,1,1)
+ax.set_xlabel('PC1', fontsize = 15)
+ax.set_ylabel('PC2', fontsize = 15)
+ax.set_title('Clusters de peliculas', fontsize = 20)
+
+color_theme = np.array(['red', 'green', 'blue', 'yellow','black'])
+ax.scatter(x = pca_clust_movies.PC1, y = pca_clust_movies.PC2, s = 50, c = color_theme[pca_clust_movies.cluster])
+
 plt.show()
+print(pca_clust_movies)'''
+
+
+#Jerarquico
+movies_jerarquico = linkage(movies_df_clean,'ward')
+#dendograma = sch.dendrogram(movies_jerarquico)
+#plt.show()
+
+clusters = fcluster(movies_jerarquico, 10, criterion='distance')
+movies_df_clean['cluster jerarquico'] = clusters
+print(clusters)
+print(movies_df_clean)
